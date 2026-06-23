a777cfd6867b33e82052289778760f5138fcb4b2
devxziki <zikieditor29@gmail.com>
2026-06-23 19:54:18 +0530

    added programs

---
+++ TY/sem-5/JAVA/oddeven.java
@@ -0,0 +1,12 @@
+class oddeven{
+	public static void main(String args[]){
+		int n=21;
+		
+		if(n%2==0){
+			System.out.println("Number is even");
+		}
+		else{
+			System.out.println("Number is odd");
+		}
+	}
+}
