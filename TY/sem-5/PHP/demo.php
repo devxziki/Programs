9e7ddab44c52377e5f73952be9a8ffabad985fd7
devxziki <zikieditor29@gmail.com>
2026-06-24 14:33:27 +0530

    added php programs

--- TY/sem-5/PHP/demo.php
+++ TY/sem-5/PHP/demo.php
@@ -4,8 +4,8 @@
 	
 		<body>
 			<h1>Welcome to my First php Program</h1>
+			<?php
+				echo "Hello World";
+			?>
 		</body>
 </html>
-<?php
-	echo "Hello World";
-?>
---
+++ TY/sem-5/PHP/simpleinteres.php
@@ -0,0 +1,12 @@
+<?php
+	
+	$price=5000;
+	$rate=3;
+	$year=3;
+	$si=$price*$rate*$year/100;
+	
+	echo "price is : ".$price."<br>";
+	echo "Rate is : ".$rate."<br>";
+	echo "Year is : ".$year."<br>";
+	echo "simple is : ".$si."<br>";
+?>
