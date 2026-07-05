35fd32b6f1e9ee42a470d77c4700a8f97bc7d420
devxziki <zikieditor29@gmail.com>
2026-07-05 08:48:38 +0000

    added 7 practical programs

--- TY/sem-5/PHP/practical-program/programs/p5.php
+++ TY/sem-5/PHP/practical-program/programs/p5.php
@@ -1,1 +1,25 @@
-//5. Write a PHP script to check whether the entered year is a leap year or not.
+<!-- 5. Write a PHP script to check whether the entered year is a leap year or not. --> 
+ <!-- 1. Write a PHP script to check whether the entered number is odd or even -->
+<html>
+	<head>
+	</head>
+		<body>
+			<form method="POST">
+				<h3> Enter Value To check That it is Leap year Or Not: </h3>
+				Enter value <input type="text" name="no1">
+				<input type="submit" name="check" value="Check Leap Year Or Not"><br>
+			</form>
+			<?php
+				if (isset($_POST['check'])) {
+					$no1 = $_POST['no1'];
+                    
+					if ($no1 % 4 == 0) {
+						echo "$no1 is An Leap Year.";
+					} 
+                    else {
+						echo "$no1 is Not An Leap Year.";
+					}
+				}
+			?>
+		</body>
+</html>
