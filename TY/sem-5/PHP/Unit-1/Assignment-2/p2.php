6666993b6d6bba41da55c9bc35f71fac772ac75a
devxziki <zikieditor29@gmail.com>
2026-07-18 11:51:25 +0530

    added php programs

---
+++ TY/sem-5/PHP/Unit-1/Assignment-2/p2.php
@@ -0,0 +1,8 @@
+<?php
+    $colors=array("red","Blue","green","black","orange","grey","light_green","white","Sky_blue","Dark_blue");
+
+	sort($colors);
+    foreach($colors as $values){
+        echo $values."<br>";
+    }
+?>
