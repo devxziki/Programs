77a0839ae90977962a0da5c496622bcf903d5bc6
devxziki <zikieditor29@gmail.com>
2026-07-22 15:53:14 +0530

    added php programs

--- TY/sem-5/PHP/Unit-1/Assignment-2/p2.php
+++ TY/sem-5/PHP/Unit-1/Assignment-2/p2.php
@@ -1,8 +1,31 @@
-<?php
-    $colors=array("red","Blue","green","black","orange","grey","light_green","white","Sky_blue","Dark_blue");
+<html>
+	<head>
+	</head>
+	<body>
+		<form method="POST">
+		<?php
+		for($i=1;$i<=10;$i++)
+		{
+			echo "Number:".($i).":<input type='text' name='num[]'><br><br>";
+		}
+		?>
+			<input type="Submit" name="check" value="Click"><br>
+		</form>
+	</body>
+</html>
 
-	sort($colors);
-    foreach($colors as $values){
-        echo $values."<br>";
-    }
+<?php
+	if(isset($_POST['check']))
+	{
+		$number=$_POST['num'];
+		
+		sort($number);
+		
+		echo "<b><h> Number in Ascending Order:</b></h><br>";
+		foreach($number as $value)
+		{
+			echo $value."<br>";
+		}
+	}
 ?>
+
