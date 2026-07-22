6f2a6553b1294aa062b41cb4d7b2073b2ec2fda1
devxziki <zikieditor29@gmail.com>
2026-07-22 20:29:16 +0530

    added java programs

--- TY/sem-5/JAVA/Practice/p8.java
+++ TY/sem-5/JAVA/Practice/p8.java
@@ -1,3 +1,12 @@
-public class p8 {
-    
+class Student { 
+    int id=101; 
+    String name="Rahul"; 
+        void display() { 
+            System.out.println(id); 
+            System.out.println(name); 
+        } 
+        public static void main(String args[]) { 
+        Student s=new Student(); 
+        s.display(); 
+    } 
 }
--- TY/sem-5/JAVA/Practice/p9.java
+++ TY/sem-5/JAVA/Practice/p9.java
@@ -1,3 +1,12 @@
-public class p9 {
-    
+class Demo { 
+    Demo() { 
+        System.out.println("Default Constructor"); 
+    } 
+    Demo(int a) { 
+        System.out.println("Parameterized Constructor="+a); 
+    } 
+    public static void main(String args[]) { 
+        Demo d1=new Demo(); 
+        Demo d2=new Demo(100); 
+    } 
 }
