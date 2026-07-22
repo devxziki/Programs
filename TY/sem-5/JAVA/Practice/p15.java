13614ea0874d28f3072ae88a0d08bac344c5cf0e
devxziki <zikieditor29@gmail.com>
2026-07-22 20:51:15 +0530

    modified programs of java

--- TY/sem-5/JAVA/Practice/p15.java
+++ TY/sem-5/JAVA/Practice/p15.java
@@ -1,3 +1,12 @@
-public class p15 {
-    
+interface Demo { 
+    void show(); 
+} 
+class p15 implements Demo { 
+    public void show() { 
+        System.out.println("Interface Method"); 
+    } 
+    public static void main(String args[]) { 
+        p15 t=new p15(); 
+        t.show(); 
+    } 
 }
